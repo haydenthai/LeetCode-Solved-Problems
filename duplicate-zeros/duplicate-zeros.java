e8308718ae5bbc5200087df753483980b5acf5c7
Hayden Thai <thaihayden@gmail.com>
2022-01-06 14:40:08 -0800

    Time: 2 ms, Memory: 43.4 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+         int[] soln = new int[arr.length];
+        
+        for(int i = 0; i < arr.length; i++){
+            soln[i] = arr[i];
+        }
+        int index = 0;
+        for(int i = 0; i < arr.length || index < arr.length; i++, index++){
+            
+            if(index >= arr.length) continue;
+            
+            if(soln[i] == 0){
+                arr[index++] = 0;
+                if(index >= arr.length) continue;
+                arr[index] = 0;
+                
+            }else{
+                arr[index] = soln[i];
+            }
+            
+        }
+        
+    }
+}

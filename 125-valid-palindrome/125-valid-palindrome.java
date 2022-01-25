b27a0904e1a061e8dfb718c04e750494a7ea2941
Hayden Thai <thaihayden@gmail.com>
2022-01-25 09:43:26 -0800

    Time: 53 ms (15.54%), Space: 43.9 MB (16.14%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -7,7 +7,7 @@ class Solution {
         int len = s.length();
         //System.out.print(s.replaceAll("\\s", "") + " ");
         for(int i = 0; i < len/2; i++){
-            System.out.print(s.charAt(i) + " ");
+            //System.out.print(s.charAt(i) + " ");
             if(s.charAt(i) != s.charAt(len-1-i)){
                 return false;
             }

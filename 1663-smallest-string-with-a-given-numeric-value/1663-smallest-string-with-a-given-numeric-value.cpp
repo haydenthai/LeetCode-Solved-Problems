f1bdf6c971849bb4d3adbecbaccb45beb0cc3e68
Hayden Thai <thaihayden@gmail.com>
2022-03-22 11:39:22 -0700

    Time: 30 ms (96.06%), Space: 21.2 MB (80.96%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -13,29 +13,10 @@ public:
         int i = n;
 
         while(k > 0){
+        
             res[--n] += min(25, k);
             k -= min(25, k);
         }
-        
-        
-//         while(i < n){
-            
-//             //if k is less than z
-//             if(k > 'z' - 'a'){
-//                 res << 'z';
-//                 k -= 27;
-//             }else{
-                
-//             }
-            
-//             i++;
-//         }
-        
-        
-        //reverse the string
-        
-        
-        
         return res;
     }
 };

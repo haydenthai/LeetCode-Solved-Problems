92bec4f6d598e0cd5ca10273882f7cbb73db6e4e
Hayden Thai <thaihayden@gmail.com>
2022-04-22 15:46:29 -0700

    Time: 10 ms (13.79%), Space: 7.9 MB (19.66%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     vector<string> cellsInRange(string s) {
         vector <string> res;
-        
+       
         int finishRow = s[4] - '0';
         int startRow = s[1] -   '0';
-        int startChar = s[0];
-        int finishChar = s[3];
+        char startChar = s[0];
+        char finishChar = s[3];
         
         while(startChar <= finishChar){
-            
             for(int i = startRow; i <= finishRow; i++){
                 stringstream ss;
-                ss << (char)startChar << (char)(i + '0');
+                ss << startChar << (char)(i + '0');
                 res.push_back(ss.str());
             }        
             startChar++;
         }
-        
-        
         return res;
     }
 };

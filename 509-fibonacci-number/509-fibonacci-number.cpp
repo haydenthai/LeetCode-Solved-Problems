58a77edfdcd0fd9c39263660fc84d6b6e87ebf45
Hayden Thai <thaihayden@gmail.com>
2022-05-20 01:25:21 -0700

    Time: 3 ms (57.79%), Space: 6 MB (41.99%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0)return 0;
-        if(n <= 2){
-            return 1;
-        }else{
-            return fib(n-1) + fib(n-2);
+        if(n==0)return 0;
+        if(n==1)return 1;
+        int res = 0;
+        int one = 0;
+        int two = 1;
+        for(int i = 1; i < n; i++){
+            
+            res = one + two;
+            one = two;
+            two = res;
         }
+        
+        return res;
         
     }
 };

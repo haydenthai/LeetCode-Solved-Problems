4dc6911e80216eac0c10d934f53a7e238f22c4b8
Hayden Thai <thaihayden@gmail.com>
2022-05-30 01:47:40 -0700

    Time: 48 ms (12.93%), Space: 16.8 MB (98.88%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,46 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-//         if(nums.size() == 1) return nums[0];
-        
-        int n = 0;
-        for (int i = 0; i < nums.size(); i++)
-            n ^= nums[i];
-        
-        return n;
-        
-//         int num, count = 0;
-//         map <int, int> mp;
-//         for(int i = 0; i < nums.size(); i++){
-//             mp[nums[i]]++;
-//             //cout << "i: " << i << " nums[i]: " << nums[i] << " " << mp[nums[i]] << endl;
-//         }
-        
-//         for(int i = 0; i < nums.size(); i++){
-//             if(mp[nums[i]] == 1){
-//                 return nums[i]; 
-//             }
-//         }
-        
-        
-//         return -1;
-     }
-};
-
-/*
-
-
-for(int i = 0; i< nums.size(); i++)
-    
-    int count = 1
-    for(int j = 0; j < nums.size(); j++){
-        if(num[i] = num[j]){
-            count++;
+        int res = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            res ^= nums[i];
         }
+        return res;
     }
-    if(count != 2){
-        return num[i];
-    }
-}
-
-*/
+};
